2f2067c410674280c60bce821294ce3983f4200d
Lucas Pedragosa <pedragosalucas.pro@gmail.com>
2024-12-02 15:30:19 +0100

    init hbs

--- Serveur.js
+++ Serveur.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const path = require('path');
 
 const app = express();
 const prisma = new PrismaClient();
-const PORT = 3001;
+const port = 3001;
+
+// Configuration de Handlebars pour Express
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
 
 app.get('/', (req, res) => {
-    res.send('Hello  test 4');
+    res.render("homePage");
 });
 
 app.listen(port, () => {
